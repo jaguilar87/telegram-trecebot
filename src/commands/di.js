5ebbbddd3723e78b704208b5877546dab2fd14e4
Jordi Aguilar <jaguilar@nicepeopleatwork.com>
2016-10-08 01:21:50 +0200

    di now uses correct chat id

--- src/commands/di.js
+++ src/commands/di.js
@@ -1,6 +1,6 @@
 module.exports = function (bot) {
   return function (msg, match) {
-    var fromId = msg.from.id
+    var fromId = msg.chat.id
     var resp = match[1]
     bot.sendMessage(fromId, resp)
   }

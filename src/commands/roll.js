e0a28ee444f4d3f0e31aa9af5f4860d036301f9a
Jordi Aguilar <jaguilar@nicepeopleatwork.com>
2016-10-09 20:43:39 +0200

    Roll validation

--- src/commands/roll.js
+++ src/commands/roll.js
@@ -4,9 +4,13 @@ let roll = new Roll()
 module.exports = function (bot) {
   return function (msg, pattern) {
     let operation = pattern[2]
-    let result = roll.roll(operation)
-    let rolled = result.rolled
-    let frase = 'Rolled: ' + rolled + ' <b>Total: ' + result.result + '</b>'
-    bot.sendMessage(msg.chat.id, frase, { parse_mode: 'HTML' })
+    if (roll.validate(operation)) {
+      let result = roll.roll(operation)
+      let rolled = result.rolled
+      let frase = 'Rolled: ' + rolled + ' <b>Total: ' + result.result + '</b>'
+      bot.sendMessage(msg.chat.id, frase, { parse_mode: 'HTML' })
+    } else {
+      bot.sendMessage(msg.chat.id, '¿Que tire qué?')
+    }
   }
 }

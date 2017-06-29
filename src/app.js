9ea1aaa2326c83d1a32655134e08c062f314e77a
Jordi Aguilar <jaguilar@nicepeopleatwork.com>
2017-06-29 12:07:38 +0200

    fix rollstats

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ bot.onText(/\/d(?:i)*(?:@\w*)*\s+(.+)/i, require('./commands/di')(bot))
 bot.onText(/\/i(?:luminame)*(?:@\w*)*/i, require('./commands/iluminame')(bot))
 bot.onText(/\/a(?:dd)*(?:@\w*)*\s+(.+)/i, require('./commands/add')(bot))
 bot.onText(/\/r(?:oll)*(?:@\w*)*\s+(.+)/i, require('./commands/roll')(bot))
-bot.onText(/\/(rs|rollstats)(?:@\w*)*\s*(.*)/i, require('./commands/rollstats')(bot))
+bot.onText(/\/(?:rs|rollstats)(?:@\w*)*\s*(.*)/i, require('./commands/rollstats')(bot))
 bot.onText(/\/f(?:uck)*(?:@\w*)*[_\s]*([^\s]*)\s*([^\s]*)\s*(.*)/i, require('./commands/fuck')(bot))
 bot.onText(/\/sn(?:@\w*)*\s*(.*)/i, require('./commands/siono')(bot))
 
